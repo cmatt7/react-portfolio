372bf9d0f74c04155b5dfeb060ebfb7dfe58032b
cmatt7 <matheusc039@hotmail.com>
2022-03-24 11:50:42 -0300

    title change on intro component

--- src/Components/Header/Introduction/Introduction.jsx
+++ src/Components/Header/Introduction/Introduction.jsx
@@ -7,7 +7,7 @@ const Introduction = () => {
         <section className="introduction-container">
             <div className="introduction-content">
                 <p><i className="fa-solid fa-terminal"></i> Hey, I'm <span className="name-highlight">Matt</span></p>
-                <h2 className="introduction-title">I'm a Front-End Developer</h2>
+                <h2 className="introduction-title">I'm a Full Stack Developer</h2>
             </div>
             <Button />
         </section>
