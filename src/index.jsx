bfd150f7f3828e2d7e860c683e404b9fb38d4d3a
Matt <matheusc039@hotmail.com>
2022-06-21 21:53:37 -0300

    new index default

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './Components/App/App';
-import PageNotFound from './Components/App/PageNotFound';
+import App from './App';
+import './global.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}/>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('app')
+	<React.StrictMode>
+		<App/>
+	</React.StrictMode>,
+	document.getElementById('app')
 );
