39f319877646aa2309ca116fb4ae361501fc0fb0
Matt <matheusc039@hotmail.com>
2022-06-25 20:26:40 -0300

    responsive header layout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,15 +10,15 @@ export default function Header() {
             {/* Social Media / Contact */}
             <Social />
 
-            <div className="flex lg:flex-row justify-center items-center h-5/6">
+            <div className="flex flex-col md:flex-row justify-center items-center h-5/6">
                 <Logo />
 
                 <div className="flex flex-col text-white">
-                    <h2 className="text-4xl font-normal">Hi, I'm <span className="font-bold animate-bounce">Matt</span></h2>
-                    <h1 className="text-5xl font-semibold mt-2.5 mb-5">Full Stack Developer</h1>
+                    <h2 className="text-4xl md:text-4xl font-normal">Hi, I'm <span className="font-bold animate-bounce">Matt</span></h2>
+                    <h1 className="text-3xl md:text-5xl font-semibold mt-2.5 mb-5">Full Stack Developer</h1>
 
                     <Button variant="contained">
-                        <a href="#about" className="font-bold relative w-full">
+                        <a href="#about" className="font-bold relative">
                             <KeyboardDoubleArrowDownOutlined /> Meet me
                         </a>
                     </Button>
