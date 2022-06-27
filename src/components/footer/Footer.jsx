9ad2698a94d06e575c7bc51e8bf540d9d4aa4b83
Matt <matheusc039@hotmail.com>
2022-06-27 13:08:10 -0300

    copyright + footer basic layout

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import Copyright from './copyright';
+
+export default function Footer() {
+    return (
+        <footer className="bg-zinc-900">
+            <Copyright/>
+        </footer>
+    )
+}
