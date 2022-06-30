70ea4ae63c41da961cee452d2ca1b97a7409095f
cmatt7 <matheusc039@hotmail.com>
2022-06-29 21:00:06 -0300

    services component layout (not done)

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,31 +1,70 @@
+import { Phonelink, DesktopWindows, Psychology } from '@mui/icons-material';
+
 export default function Services() {
     const cardStyle = "artboard phone-1 bg-black my-14 mx-auto md:mx-10 lg:mx-20 rounded-md drop-shadow-lg"
 
     return (
-        <section className="bg-zinc-900" data-aos="fade-up">
-            <div className="custom-shape-divider-bottom-1656335499">
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className="shape-fill"></path>
-                    <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className="shape-fill"></path>
-                    <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className="shape-fill"></path>
-                </svg>
+        <section className="py-20" data-aos="fade-down">
+
+            <div className="text-center">
+                <h2 className="text-lg font-semibold uppercase text-slate-300">Services</h2>
+                <h1 className="text-2xl md:text-3xl font-bold pt-4">How may I help you?</h1>
             </div>
 
-            <h1 className="flex justify-center text-2xl font-bold pt-16">Services</h1>
+            <main className="flex flex-col lg:flex-row justify-center my-14">
+                <CardInfo
+                    title="Responsive"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porttitor molestie neque, eu cursus nisi aliquet ac. 
+                    In varius diam auctor quam lobortis euismod. Donec ut turpis sem. 
+                    Quisque accumsan augue velit, eu ultricies ipsum tristique quis. 
+                    Quisque tincidunt leo nec quam malesuada, et porttitor arcu porttitor."
+                >
+                    <Phonelink className="text-sky-600" fontSize="large" />
+                </CardInfo>
 
-            <main className="flex flex-col md:flex-row flex-wrap justify-center items-center">
-                <div className={cardStyle}>
-                    320×568
-                </div>
+                <CardInfo
+                    title="Dynamic"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porttitor molestie neque, eu cursus nisi aliquet ac. 
+                    In varius diam auctor quam lobortis euismod. Donec ut turpis sem. 
+                    Quisque accumsan augue velit, eu ultricies ipsum tristique quis. 
+                    Quisque tincidunt leo nec quam malesuada, et porttitor arcu porttitor."
+                >
+                    <DesktopWindows className="text-sky-600" fontSize="large" />
+                </CardInfo>
 
-                <div className={cardStyle}>
-                    320×568
-                </div>
-
-                <div className={cardStyle}>
-                    320×568
-                </div>
+                <CardInfo
+                    title="Mentorship"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porttitor molestie neque, eu cursus nisi aliquet ac. 
+                    In varius diam auctor quam lobortis euismod. Donec ut turpis sem. 
+                    Quisque accumsan augue velit, eu ultricies ipsum tristique quis. 
+                    Quisque tincidunt leo nec quam malesuada, et porttitor arcu porttitor."
+                >
+                    <Psychology className="text-sky-600" fontSize="large" />
+                </CardInfo>
             </main>
+
         </section>
+    )
+}
+
+export function CardInfo({ children, title, text }) {
+    return (
+        <div className="flex flex-col items-center my-6 px-8 md:px-24 lg:px-0 lg:mx-8 xl:mx-24" data-aos="fade-down">
+            {/* Icon */}
+            <span className="flex justify-center items-center w-20 h-20 bg-white rounded-full">
+                {children}
+            </span>
+            {/* Icon */}
+
+            {/* Title */}
+            <h2 className="text-xl font-semibold my-4">
+                {title}
+            </h2>
+            {/* Title */}
+
+            {/* Text */}
+            <p className="text-md text-justify">{text}</p>
+            {/* Text */}
+        </div>
     )
 }
