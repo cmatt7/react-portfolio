d7be2682f11c2e7f895241f63c604b500187c5d9
Matt <matheusc039@hotmail.com>
2023-02-03 18:45:19 -0300

    vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-// AOS
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-// Components
-import Header from "./components/header/Header";
-import About from "./components/about/About";
-import Experience from "./components/experience/Experience";
-import Services from './components/services/Services';
-import Projects from './components/projects/Projects';
-import Footer from './components/footer/Footer';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-export default function App() {
-    AOS.init({
-        duration: 1500
-    });
+function App() {
+  const [count, setCount] = useState(0)
 
-    return (
-        <>
-            <Header/>
-            <About/>
-            <Experience/>
-            <Services/>
-            <Projects/>
-            <Footer/>
-        </>
-    )
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
+
+export default App
