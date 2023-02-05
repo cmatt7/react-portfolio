5412fa2d7c249a61150212200bc208fd900e7e42
Matt <matheusc039@hotmail.com>
2023-02-05 17:41:26 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import Menu from "./components/Menu.component";
+
+// Pages
+import Home from "./pages/Home.component";
+import About from "./pages/About.component";
+import Services from "./pages/Services.components";
+
 export default function App() {
     return (
-        <>
+        <BrowserRouter>
+            <Menu/>
 
-        </>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/services" element={<Services/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
