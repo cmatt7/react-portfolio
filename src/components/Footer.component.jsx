e714615d953146b586d96d611f270fe614fd1993
Matt <matheusc039@hotmail.com>
2023-02-07 01:16:51 -0300

    footer typo

--- src/components/Footer.component.jsx
+++ src/components/Footer.component.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
-        <footer className="px-6lg:px-20 2xl:px-40 py-8 mt-10">
+        <footer className="px-6 lg:px-20 2xl:px-40 py-8 mt-10">
             <hr className="my-3 border-gray-500"/>
 
             <span
-                className="block text-sm text-gray-500 sm:text-center"
+                className="block text-sm text-gray-500 text-center"
             >
                 © {new Date().getFullYear()} Matt. All Rights Reserved.
             </span>
