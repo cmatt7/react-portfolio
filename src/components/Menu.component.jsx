f50ecb00cedc508186a42f4cd74f271b58a32142
Matt <matheusc039@hotmail.com>
2023-02-07 15:01:25 -0300

    mobile menu

--- src/components/Menu.component.jsx
+++ src/components/Menu.component.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 // Assets
 import Logo from "../assets/logo.png";
-import { AiOutlineFolderOpen, AiOutlineHome, AiOutlineRest, AiOutlineTeam } from "react-icons/ai"
+import { AiOutlineFolderOpen, AiOutlineHome, AiOutlineRest, AiOutlineTeam } from "react-icons/ai";
+import { BiMenuAltLeft, BiMenuAltRight } from "react-icons/bi";
 import { RiSuitcaseLine } from "react-icons/ri";
 
 export default function Menu() {
     const [open, setOpen] = useState(false)
     const [scroll, setScroll] = useState(false)
-
     const changeMenuScroll = () => {
-        if(window.scrollY > 0) {
+        if (window.scrollY > 0) {
             setScroll(true)
         } else {
             setScroll(false)
@@ -55,31 +55,42 @@ export default function Menu() {
     window.addEventListener('scroll', changeMenuScroll);
 
     return (
-        <nav 
-            className={`
-                hidden md:flex
+        <>
+            <div className={`${open ? "bg-blue-600" : null} block md:hidden px-6 py-4`}>
+                {open ? (
+                    <BiMenuAltRight className="text-white text-2xl cursor-pointer" onClick={() => setOpen(false)} />
+                ) : (
+                    <BiMenuAltLeft className="text-white text-2xl cursor-pointer" onClick={() => setOpen(true)} />
+                )}
+            </div>
+
+            <nav
+                className={`
+                ${open ? "flex bg-blue-600" : "hidden"} md:flex
                 sticky top-0 w-full px-5 lg:px-12 2xl:px-40 py-3 justify-between items-center
                 ${scroll ? "bg-blue-600 backdrop-blur-md bg-opacity-20" : null} z-50
             `}>
-            <figure className="hidden lg:block">
-                <Link to="/">
-                    <img src={Logo} alt="Matt - Logo" className="h-12" />
-                </Link>
-            </figure>
-
-            <ul className="flex gap-x-5">
-                {items.map(i => (
-                    <Link
-                        to={i.path}
-                        key={i.id}
-                        className="flex items-center justify-center gap-x-1 text-white 
-                        text-lg hover:text-blue-600 duration-300 ease-in-out"
-                    >
-                        {i.icon}
-                        {i.title}
+                <figure className="hidden lg:block">
+                    <Link to="/">
+                        <img src={Logo} alt="Matt - Logo" className="h-12" />
                     </Link>
-                ))}
-            </ul>
-        </nav>
+                </figure>
+
+                <ul className={`${open ? "flex flex-col items-start w-full" : null} flex gap-x-5 gap-y-5`}>
+                    {items.map(i => (
+                        <Link
+                            to={i.path}
+                            key={i.id}
+                            className="flex items-center justify-center gap-x-1 text-white 
+                        text-lg hover:text-blue-600 duration-300 ease-in-out"
+                            onClick={() => setOpen(false)}
+                        >
+                            {i.icon}
+                            {i.title}
+                        </Link>
+                    ))}
+                </ul>
+            </nav>
+        </>
     )
 }
